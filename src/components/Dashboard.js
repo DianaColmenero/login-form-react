96c78448bf42ca86607d03745b053d18a0292482
Developer <developer@IM-M11-877.local>
2018-06-25 15:36:13 -0500

    login and dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar.js'
+import './css/Dashboard.css';
+
+class Dashboard extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<h2>Dashboard</h2>
+
+			</div>
+		);
+	}
+}
+
+export default Dashboard;
